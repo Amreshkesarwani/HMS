8b2a536583f484239544ff268db9d0902fd8c7da
dreamboy07 <dreamboy07@gmail.com>
2021-04-01 23:32:52 +0530

    Add user mgt

---
+++ BusinessEntities/Common/DropDownListItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessEntities.Common
+{
+  public  class DropDownListItem
+    {
+        public string Text { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ HMS/Areas/UserManagement/Controllers/UserManagementController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HMS.Areas.UserManagement.Controllers
+{
+    [Area("UserManagement")]
+    [Route("UserManagement/[controller]/[action]")]
+    public class UserManagementController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult Add()
+        //{
+        //    return View();
+        //}
+    }
+}
